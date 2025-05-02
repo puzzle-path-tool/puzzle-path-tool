579eea1b686eb6066cfc5c6ad6351f808b353b88
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2025-05-02 21:15:52 +0200

    Add Dependencies

---
+++ deps.sh
@@ -0,0 +1,5 @@
+cd crates/puzzfmt_lib_wasm
+# cargo add --path ../common
+# cargo add --path ../common_macros
+cargo add --path ../puzzfmt_lib
+# cargo add --path ../puzzpt_lib
---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir crates
+cd crates
+cargo new --lib common
+cargo new --lib common_macros
+cargo new --lib puzzfmt_lib
+cargo new --lib puzzfmt_lib_c
+cargo new --lib puzzfmt_lib_wasm
+cargo new --lib puzzpt_lib
+cargo new --lib puzzpt_lib_c
+cargo new --lib puzzpt_lib_wasm
+cargo new --bin puzzpt_program_cli
