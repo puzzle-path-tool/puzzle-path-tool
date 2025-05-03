c26030f02d12de346c8e1a29f936a32b661d6850
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2025-05-03 23:19:12 +0200

    Configure C Builds

--- crates/puzzle_base_build/src/lib.rs
+++ crates/puzzle_base_build/src/lib.rs
@@ -1,14 +1,85 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{
+    env::{self, VarError},
+    error::Error,
+    fmt::Display,
+    path::PathBuf,
+};
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum BuildEnvError {
+    InvalidEnvVar(VarError, String),
+    InvalidDirStructure(PathBuf),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Display for BuildEnvError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BuildEnvError::InvalidEnvVar(err, var) => {
+                write!(f, "Env var {} is invalid: {}", var, err)
+            }
+            BuildEnvError::InvalidDirStructure(path) => write!(
+                f,
+                "Dir Structure is invalid: {}",
+                path.to_str().unwrap_or_default()
+            ),
+        }
     }
+}
+impl Error for BuildEnvError {}
+
+pub fn crate_dir() -> Result<PathBuf, BuildEnvError> {
+    dir_from_env_var("CARGO_MANIFEST_DIR")
+}
+
+pub fn out_dir() -> Result<PathBuf, BuildEnvError> {
+    dir_from_env_var("OUT_DIR")
+}
+
+pub fn target_dir() -> Result<PathBuf, BuildEnvError> {
+    let out_dir = out_dir()?;
+    out_dir
+        .ancestors()
+        .nth(3)
+        .map(PathBuf::from)
+        .ok_or_else(|| BuildEnvError::InvalidDirStructure(out_dir))
+}
+
+pub fn target_file_name() -> Result<String, BuildEnvError> {
+    let file_prefix = match target_os()?.as_str() {
+        "windows" => "",
+        _ => "lib",
+    };
+    Ok(format!(
+        "{}{}",
+        file_prefix,
+        package_name()?.replace("-", "_")
+    ))
+}
+
+pub fn beside_file_path(suffix: &str) -> Result<PathBuf, BuildEnvError> {
+    Ok(target_dir()?.join(format!("{}{}", target_file_name()?, suffix)))
+}
+
+pub fn package_name() -> Result<String, BuildEnvError> {
+    string_from_env_var("CARGO_PKG_NAME")
+}
+
+pub fn target_os() -> Result<String, BuildEnvError> {
+    string_from_env_var("CARGO_CFG_TARGET_OS")
+}
+
+fn string_from_env_var(var: &str) -> Result<String, BuildEnvError> {
+    env::var(var).map_err(|err| BuildEnvError::InvalidEnvVar(err, String::from(var)))
+}
+
+fn dir_from_env_var(var: &str) -> Result<PathBuf, BuildEnvError> {
+    string_from_env_var(var).map(|v| PathBuf::from(v))
+}
+
+pub fn rerun_if_changed_any() {
+    rerun_if_changed("**/*");
+}
+
+pub fn rerun_if_changed(pattern: &str) {
+    println!("cargo:rerun-if-changed={}", pattern);
 }
