ceb7616a76405879225761f33d2b1eb3f06bc38e
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2025-05-03 02:29:18 +0200

    Add Lua

---
+++ scripts/core/example1.lua
@@ -0,0 +1,18 @@
+---@class Z
+---@field a string
+
+
+---@type Z
+local y = { a = "" }
+
+---@type Z
+local y = { b = "" }
+
+---@type Z
+local y2 = { a = "" }
+
+
+print(y.a)
+print(y.a)
+print(y:a)
+print(y.c)
---
+++ scripts/core/type2.lua
@@ -0,0 +1,4 @@
+---@meta
+
+---@class Y<
+---@field a strin
