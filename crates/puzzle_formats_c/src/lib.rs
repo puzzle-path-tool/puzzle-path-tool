91730352d1d9c4f1c68c3dc52fe46a00a808b548
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2025-05-04 19:23:05 +0200

    Add more clippy linting

--- crates/puzzle_formats_c/src/lib.rs
+++ crates/puzzle_formats_c/src/lib.rs
@@ -3,11 +3,11 @@
 ///
 #[unsafe(no_mangle)]
 pub extern "C" fn star_operation(left: u64, right: u64) -> u64 {
-    puzzle_formats::star_operation(left, right)
+    left + right
 }
 
 /// Does Wild Magic
 #[unsafe(no_mangle)]
 pub extern "C" fn star_operation23(left: u64, right: u64) -> u64 {
-    puzzle_formats::star_operation(left, right)
+    left + right
 }
