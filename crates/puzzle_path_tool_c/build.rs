fb18ef2eb27e071ea002be29854896cd41353baf
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2025-05-05 14:46:29 +0200

    Add anyhow

--- crates/puzzle_path_tool_c/build.rs
+++ crates/puzzle_path_tool_c/build.rs
@@ -1,13 +1,11 @@
-use std::error::Error;
-
 use puzzle_core_build as cb;
 
 fn main() {
     generate_bindings().unwrap_or_else(|err| panic!("{}", err));
 }
 
-fn generate_bindings() -> Result<(), Box<dyn Error>> {
-    let config = cbindgen::Config::from_file("cbindgen.toml")?;
+fn generate_bindings() -> anyhow::Result<()> {
+    let config = cbindgen::Config::from_file("cbindgen.toml").map_err(anyhow::Error::msg)?;
     let out_file = cb::beside_file_path(".h")?;
 
     cbindgen::generate_with_config(cb::crate_dir()?, config)?.write_to_file(out_file);
