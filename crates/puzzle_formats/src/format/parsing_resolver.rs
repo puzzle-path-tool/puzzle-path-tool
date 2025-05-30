68deae74026c084d5002a9f16b1d4582c629e7d1
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2025-05-30 13:32:38 +0200

    Change Resolver

--- crates/puzzle_formats/src/format/parsing_resolver.rs
+++ crates/puzzle_formats/src/format/parsing_resolver.rs
@@ -7,3 +7,9 @@ pub trait ParsingResolver {
 }
 
 //TODO, add ability to fail, and stepwise transformation (Maybe Some kind of Builder?)
+
+// Puzzle Operations:
+
+// fn construct_logical(full: FullPuzzle) -> LogicalPuzzle;
+// fn construct_visual(full: FullPuzzle) -> VisualPuzzle;
+// fn from_logical(logical: LogicalPuzzle) -> FullPuzzle;
