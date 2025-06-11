8620d340dbf5d8646d6b4b75fa83cd7853f00e7e
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2025-06-11 17:11:50 +0200

    Fix

--- crates/puzzle_path_tool/src/lib.rs
+++ crates/puzzle_path_tool/src/lib.rs
@@ -1,10 +1,8 @@
-use std::sync::Arc;
-
-use mlua::AsChunk;
 use quickjs_runtime::{
     builder::QuickJsRuntimeBuilder,
     jsutils::{JsError, Script, ScriptPreProcessor},
-    typescript::{TargetVersion, TypeScriptTranspiler}, values::JsValueFacade,
+    typescript::{TargetVersion, TypeScriptTranspiler},
+    values::JsValueFacade,
 };
 
 #[must_use]
@@ -12,10 +10,27 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+struct TypeScriptPreProcessor {
+    transpiler: TypeScriptTranspiler,
+}
+
+impl TypeScriptPreProcessor {
+    fn new() -> Self {
+        Self {
+            transpiler: TypeScriptTranspiler::new(TargetVersion::Es2022, false, false, false),
+        }
+    }
+}
+
+impl ScriptPreProcessor for TypeScriptPreProcessor {
+    fn process(&self, script: &mut Script) -> Result<(), JsError> {
+        self.transpiler.transpile_script(script)
+    }
+}
+
 fn do_stuff() -> Result<JsValueFacade, JsError> {
-    let transpiler = TypeScriptTranspiler::new(TargetVersion::Es2022, false, false, false);
     let rt = QuickJsRuntimeBuilder::new()
-        .script_pre_processor(transpiler)
+        .script_pre_processor(TypeScriptPreProcessor::new())
         .build();
 
     let filename = "script.ts";
