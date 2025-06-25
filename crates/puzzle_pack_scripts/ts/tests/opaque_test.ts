9219da0b90a0359872226e8c290fb4e0c7e7ed4d
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2025-06-25 15:51:27 +0200

    Add Other Types

--- crates/puzzle_pack_scripts/ts/tests/opaque_test.ts
+++ crates/puzzle_pack_scripts/ts/tests/opaque_test.ts
@@ -23,7 +23,7 @@ export function doStuff(value: A) {
 
 // Opaque Data test 2
 
-type RecordType = Record<string, unknown>;
+type RecordType<T = unknown> = Record<string, T>;
 
 type BData<T extends RecordType> = { name: string; fields: T };
 type BFields<T extends RecordType> = {
@@ -88,21 +88,32 @@ function wrapFields<const T extends RecordType>(
     obj: T,
     id: string,
 ): BFields<T> {
-    const x: [keyof BFields<T>, BFields<T>[keyof BFields<T>]][] =
-        Object.entries(obj).map(([k, v]) => {
-            const key = k as keyof T;
-            const value = v as T[keyof T];
+    return mapFields<T, BFields<T>>(obj, (key, value) => [
+        [
+            key,
+            {
+                id: id,
+                value: value,
+            },
+        ],
+    ]);
+}
 
-            return [
-                key,
-                {
-                    id: id,
-                    value: value,
-                },
-            ];
-        });
+function mapFields<TFrom extends RecordType, TTo extends RecordType>(
+    obj: TFrom,
+    map_key: (
+        key: keyof TFrom,
+        value: TFrom[keyof TFrom],
+    ) => [key: keyof TTo, value: TTo[keyof TTo]][],
+): TTo {
+    const entries = Object.entries(obj).flatMap(([k, v]) => {
+        const key: keyof TFrom = k;
+        const value = v as TFrom[keyof TFrom];
 
-    return Object.fromEntries(x) as BFields<T>;
+        return map_key(key, value);
+    });
+
+    return Object.fromEntries(entries) as TTo;
 }
 
 class BWrapper<T extends RecordType> {
@@ -140,3 +151,205 @@ console.log(b.x.value);
 
 console.log(b instanceof BWrapper);
 console.log(BWrapper.unwrap(b));
+
+// Field Declarations
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type FieldDeclaration =
+    | IntFieldDeclaration
+    | BoolFieldDeclaration
+    | EnumFieldDeclaration<any>
+    | ObjectFieldDeclaration<any>
+    | ArrayFieldDeclaration<any>;
+/* eslint-enable @typescript-eslint/no-explicit-any */
+
+class FieldDeclUtil {
+    static isInt(value: unknown): value is IntFieldDeclaration {
+        return value instanceof IntFieldDeclarationWrapper;
+    }
+    static isBool(value: unknown): value is BoolFieldDeclaration {
+        return value instanceof BoolFieldDeclarationWrapper;
+    }
+    static isEnum(
+        value: unknown,
+    ): value is EnumFieldDeclaration<readonly string[]> {
+        return value instanceof EnumFieldDeclarationWrapper;
+    }
+    static isObject(
+        value: unknown,
+    ): value is ObjectFieldDeclaration<RecordType<FieldDeclaration>> {
+        return value instanceof ObjectFieldDeclarationWrapper;
+    }
+    static isArray(
+        value: unknown,
+    ): value is ArrayFieldDeclaration<FieldDeclaration> {
+        return value instanceof ArrayFieldDeclarationWrapper;
+    }
+}
+
+// Int Field Declaration
+
+type IntFieldDeclarationData = object;
+class IntFieldDeclarationWrapper {
+    private readonly _private_data: IntFieldDeclarationData;
+    private constructor(data: IntFieldDeclarationData) {
+        this._private_data = data;
+    }
+    static unwrap(value: IntFieldDeclaration): IntFieldDeclarationData {
+        return value._private_data;
+    }
+    static wrap(value: IntFieldDeclarationData): IntFieldDeclaration {
+        return new IntFieldDeclarationWrapper(value);
+    }
+}
+export type IntFieldDeclaration = IntFieldDeclarationWrapper;
+
+// Bool Field Declaration
+
+type BoolFieldDeclarationData = object;
+class BoolFieldDeclarationWrapper {
+    private readonly _private_data: BoolFieldDeclarationData;
+    private constructor(data: BoolFieldDeclarationData) {
+        this._private_data = data;
+    }
+    static unwrap(value: BoolFieldDeclaration): BoolFieldDeclarationData {
+        return value._private_data;
+    }
+    static wrap(value: BoolFieldDeclarationData): BoolFieldDeclaration {
+        return new BoolFieldDeclarationWrapper(value);
+    }
+}
+export type BoolFieldDeclaration = BoolFieldDeclarationWrapper;
+
+// Enum Field Declaration
+
+interface EnumFieldDeclarationData<T extends readonly string[]> {
+    values: T;
+}
+class EnumFieldDeclarationWrapper<T extends readonly string[]> {
+    private readonly _private_data: EnumFieldDeclarationData<T>;
+    private constructor(data: EnumFieldDeclarationData<T>) {
+        this._private_data = data;
+    }
+    static unwrap<T extends readonly string[]>(
+        value: EnumFieldDeclaration<T>,
+    ): EnumFieldDeclarationData<T> {
+        return value._private_data;
+    }
+    static wrap<const T extends readonly string[]>(
+        value: EnumFieldDeclarationData<T>,
+    ): EnumFieldDeclaration<T> {
+        return new EnumFieldDeclarationWrapper(value);
+    }
+}
+
+export type EnumFieldDeclaration<T extends readonly string[]> =
+    EnumFieldDeclarationWrapper<T>;
+
+// Object Field Declaration
+
+interface ObjectFieldDeclarationData<T extends RecordType<FieldDeclaration>> {
+    fields: T;
+}
+class ObjectFieldDeclarationWrapper<T extends RecordType<FieldDeclaration>> {
+    private readonly _private_data: ObjectFieldDeclarationData<T>;
+    private constructor(data: ObjectFieldDeclarationData<T>) {
+        this._private_data = data;
+    }
+    static unwrap<T extends RecordType<FieldDeclaration>>(
+        value: ObjectFieldDeclaration<T>,
+    ): ObjectFieldDeclarationData<T> {
+        return value._private_data;
+    }
+    static wrap<const T extends RecordType<FieldDeclaration>>(
+        value: ObjectFieldDeclarationData<T>,
+    ): ObjectFieldDeclaration<T> {
+        return new ObjectFieldDeclarationWrapper(value);
+    }
+}
+export type ObjectFieldDeclaration<T extends RecordType<FieldDeclaration>> =
+    ObjectFieldDeclarationWrapper<T>;
+
+// Array Field Declaration
+
+interface ArrayFieldDeclarationData<T extends FieldDeclaration> {
+    item: T;
+}
+class ArrayFieldDeclarationWrapper<T extends FieldDeclaration> {
+    private readonly _private_data: ArrayFieldDeclarationData<T>;
+    private constructor(data: ArrayFieldDeclarationData<T>) {
+        this._private_data = data;
+    }
+    static unwrap<T extends FieldDeclaration>(
+        value: ArrayFieldDeclaration<T>,
+    ): ArrayFieldDeclarationData<T> {
+        return value._private_data;
+    }
+    static wrap<const T extends FieldDeclaration>(
+        value: ArrayFieldDeclarationData<T>,
+    ): ArrayFieldDeclaration<T> {
+        return new ArrayFieldDeclarationWrapper(value);
+    }
+}
+export type ArrayFieldDeclaration<T extends FieldDeclaration> =
+    ArrayFieldDeclarationWrapper<T>;
+
+// Test
+
+const field = {
+    decl: {
+        int: IntFieldDeclarationWrapper,
+        bool: BoolFieldDeclarationWrapper,
+        enum: EnumFieldDeclarationWrapper,
+        obj: ObjectFieldDeclarationWrapper,
+        arr: ArrayFieldDeclarationWrapper,
+    },
+} as const;
+
+const b1 = field.decl.obj.wrap({
+    fields: {
+        a: field.decl.int.wrap({}),
+        b: field.decl.obj.wrap({
+            fields: {
+                c: field.decl.int.wrap({}),
+                d: field.decl.int.wrap({}),
+                e: field.decl.obj.wrap({
+                    fields: {
+                        f: field.decl.int.wrap({}),
+                        g: field.decl.int.wrap({}),
+                    },
+                }),
+            },
+        }),
+        aa: field.decl.arr.wrap({
+            item: field.decl.int.wrap({}),
+        }),
+        ab: field.decl.arr.wrap({
+            item: field.decl.obj.wrap({
+                fields: {
+                    a: field.decl.arr.wrap({
+                        item: field.decl.arr.wrap({
+                            item: field.decl.int.wrap({}),
+                        }),
+                    }),
+                },
+            }),
+        }),
+        ac: field.decl.obj.wrap({
+            fields: {
+                a: field.decl.enum.wrap({
+                    values: ["1", "2", "3"],
+                }),
+            },
+        }),
+    },
+});
+
+const b2 = field.decl.obj.wrap({
+    fields: {
+        a: field.decl.enum.wrap({
+            values: ["1", "2", "3"],
+        }),
+        b: field.decl.bool.wrap({}),
+    },
+});
