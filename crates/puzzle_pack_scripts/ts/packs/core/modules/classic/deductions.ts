bb73520ccffd8801591776ec98e0e04e197a7f98
Max Petersen <max@petersen-online.eu>
2025-07-02 15:35:07 +0200

    Add SplitSet

--- crates/puzzle_pack_scripts/ts/packs/core/modules/classic/deductions.ts
+++ crates/puzzle_pack_scripts/ts/packs/core/modules/classic/deductions.ts
@@ -1,57 +1,57 @@
 import { classic_mod } from "../classic_mod";
 
 declare const field: any;
+declare const array: any;
+declare const int: any;
 type TODO = any;
 const todo = "TODO";
 
 function position() {
-    return field.decl.obj.wrap(
-        {
-            x: field.decl.int.wrap({}),
-            y: field.decl.int.wrap({}),
-        },
-    );
+    return field.decl.obj.wrap({
+        x: int.field,
+        y: int.field,
+    });
 }
 
 export const allowed_values = classic_mod.deduction({
     name: "allowed_values",
     data: {
-        values: field.decl.arr.wrap(field.decl.int.wrap({})),
+        values: array.field(int.field),
         cell: position(),
     },
-})
+});
 
 export const required_values = classic_mod.deduction({
     name: "required_values",
     data: {
-        value: field.decl.int.wrap({}),
-        cells: field.decl.arr.wrap(position()),
+        value: int.field,
+        cells: array.field(position()),
     },
-})
+});
 
 export const matching_cells = classic_mod.deduction({
     name: "matching_cells",
     data: {
-        cells: field.decl.arr.wrap(position()),
+        cells: array.field(position()),
     },
-})
+});
 
 export const non_repeat_set = classic_mod.deduction({
     name: "non_repeat_set",
     data: {
-        cells: field.decl.arr.wrap(position()),
+        cells: array.field(position()),
     },
-})
+});
 
 export const full_set = classic_mod.deduction({
     name: "full_set",
     data: {
-        values: field.decl.arr.wrap(field.decl.int.wrap({})),
-        cells: field.decl.arr.wrap(position()),
+        values: array.field(int.field),
+        cells: array.field(position()),
     },
-})
+});
 
 export const error = classic_mod.deduction({
     name: "error",
-    data: { },
-})
+    data: {},
+});
--- crates/puzzle_pack_scripts/ts/packs/core/modules/classic/split_set_step.ts
+++ crates/puzzle_pack_scripts/ts/packs/core/modules/classic/split_set_step.ts
@@ -1,5 +1,10 @@
 import { classic_mod } from "../classic_mod";
-import { allowed_values, full_set, required_values } from "./deductions";
+import {
+    allowed_values,
+    full_set,
+    non_repeat_set,
+    required_values,
+} from "./deductions";
 
 declare const quantor: any;
 declare const set: any;
@@ -8,49 +13,144 @@ declare const int: any;
 type TODO = any;
 const todo = "TODO";
 
-const split_fullset_step = classic_mod.step({
-    name: "split_fullset",
+const split_full_set_step = classic_mod.step({
+    name: "split_full_set",
     logic: (matcher: TODO, emitter: TODO) => {
-        const set1 = matcher.pool.get_one(full_set)
+        const set1 = matcher.pool.get_one(full_set);
 
-        const allowed_values_set = matcher.pool.get_many(allowed_values)
-        const values = matcher.pool.get_many(int)
+        const allowed_values_set = matcher.pool.get_many(allowed_values);
+        const values = matcher.get_many(int);
         matcher.where(
             quantor.all((matcher: TODO) => {
-                const value = matcher.get_one(allowed_values)
-                matcher.where(set.do(value, "element of", allowed_values_set))
+                const value = matcher.get_one(allowed_values);
+                matcher.where(set.do(value, "element of", allowed_values_set));
 
-                matcher.require(set.do(value.cell, "element of", set1.cells))
-                matcher.require(set.do(value.values, "subset of", values))
-            })
-        )
-        matcher.require(cmp.do(
-            values.size, "==", allowed_values_set.size
-        ))
+                matcher.require(set.do(value.cell, "element of", set1.cells));
+                matcher.require(set.do(value.values, "subset of", values));
+            }),
+        );
+        matcher.require(cmp.do(values.size, "==", allowed_values_set.size));
 
-        const set2 = matcher.pool.get_one(full_set)
+        const set2 = {
+            cells: allowed_values_set.map((x: TODO) => {
+                x.cell;
+            }),
+            values: values,
+        };
+
+        emitter.emit(full_set, [
+            set2,
+            {
+                cells: set.do(set1.cells, "without", set2.cells),
+                values: set.do(set1.values, "without", set2.values),
+            },
+        ]);
+
+        const outer_allowed_values_set = matcher.get_many(allowed_values_set);
         matcher.require(
-            cmp.do(set2.values, "==", values)
-        )
+            cmp.do(
+                set.do(set1.cells, "without", set2.cells),
+                "==",
+                outer_allowed_values_set.map((x: TODO) => {
+                    x.cell;
+                }),
+            ),
+        );
         matcher.require(
             quantor.all((matcher: TODO) => {
-                const value = matcher.get_one(allowed_values)
-                matcher.where(set.do(value, "element of", allowed_values_set))
+                const value1 = matcher.get_one(allowed_values);
+                matcher.where(
+                    set.do(value1, "element of", outer_allowed_values_set),
+                );
+                const value2 = matcher.get_one(allowed_values);
+                matcher.pool.where(
+                    set.do(
+                        value2.cell,
+                        "element of",
+                        outer_allowed_values_set.map((x: TODO) => {
+                            x.cell;
+                        }),
+                    ),
+                );
 
-                matcher.require(set.do(value.cell, "element of", set2.cells))
-            })
-        )
+                matcher.require(
+                    cmp.do(
+                        value1.values,
+                        "==",
+                        set.do(value2.values, "without", set2.values),
+                    ),
+                );
+            }),
+        );
 
-        const set3 = matcher.pool.get_one(full_set)
+        emitter.emit(allowed_values, outer_allowed_values_set);
+    },
+});
+
+const split_non_repeat_set_step = classic_mod.step({
+    name: "split_non_repeat_set",
+    logic: (matcher: TODO, emitter: TODO) => {
+        const set1 = matcher.pool.get_one(non_repeat_set);
+
+        const allowed_values_set = matcher.pool.get_many(allowed_values);
+        const values = matcher.get_many(int);
+        matcher.where(
+            quantor.all((matcher: TODO) => {
+                const value = matcher.get_one(allowed_values);
+                matcher.where(set.do(value, "element of", allowed_values_set));
+
+                matcher.require(set.do(value.cell, "element of", set1.cells));
+                matcher.require(set.do(value.values, "subset of", values));
+            }),
+        );
+        matcher.require(cmp.do(values.size, "==", allowed_values_set.size));
+
+        const set2 = {
+            cells: allowed_values_set.map((x: TODO) => {
+                x.cell;
+            }),
+            values: values,
+        };
+
+        emitter.emit(full_set, [set2]);
+
+        const outer_allowed_values_set = matcher.get_many(allowed_values_set);
         matcher.require(
-            cmp.do(set3.values, "==", set.do(set1.values, "without", set2.values))
-        )
+            cmp.do(
+                set.do(set1.cells, "without", set2.cells),
+                "==",
+                outer_allowed_values_set.map((x: TODO) => {
+                    x.cell;
+                }),
+            ),
+        );
         matcher.require(
-            cmp.do(set3.values, "==", set.do(set1.cells, "without", set2.cells))
-        )
+            quantor.all((matcher: TODO) => {
+                const value1 = matcher.get_one(allowed_values);
+                matcher.where(
+                    set.do(value1, "element of", outer_allowed_values_set),
+                );
+                const value2 = matcher.get_one(allowed_values);
+                matcher.pool.where(
+                    set.do(
+                        value2.cell,
+                        "element of",
+                        outer_allowed_values_set.map((x: TODO) => {
+                            x.cell;
+                        }),
+                    ),
+                );
 
-        const outer_allowed_values_set = todo
+                matcher.require(
+                    cmp.do(
+                        value1.values,
+                        "==",
+                        set.do(value2.values, "without", set2.values),
+                    ),
+                );
+            }),
+        );
 
-        emitter.emit(set2, set3, outer_allowed_values_set)
-    }
-})
+        emitter.emit(allowed_values, outer_allowed_values_set);
+    },
+});
