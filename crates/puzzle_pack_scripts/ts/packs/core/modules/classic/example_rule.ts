720038b96b1f78586df917df7265caaf3579f375
Max Petersen <max@petersen-online.eu>
2025-07-09 14:44:54 +0200

    Remove empty examples

--- crates/puzzle_pack_scripts/ts/packs/core/modules/classic/example_rule.ts
+++
@@ -1,5 +0,0 @@
-import { classic_mod } from "../classic_mod";
-
-export const example_rule = classic_mod.rule({
-    name: "example",
-});
--- crates/puzzle_pack_scripts/ts/packs/core/modules/classic/example_test.ts
+++
@@ -1,5 +0,0 @@
-import { classic_mod } from "../classic_mod";
-
-export const example_ded = classic_mod.deduction({
-    name: "example",
-});
