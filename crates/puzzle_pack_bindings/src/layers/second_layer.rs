17c6585a520f031b1282114593a75364e8bb88d0
Max Petersen <max@petersen-online.eu>
2025-07-23 16:22:15 +0200

    Second Layer Datastructure

---
+++ crates/puzzle_pack_bindings/src/layers/second_layer.rs
@@ -0,0 +1,188 @@
+use std::sync::atomic;
+
+use itertools::Itertools;
+
+#[derive(Debug, Clone, Copy)]
+pub struct TableId {
+    value: usize,
+}
+
+static COUNTER: atomic::AtomicUsize = atomic::AtomicUsize::new(1);
+impl TableId {
+    fn new() -> TableId {
+        TableId {
+            value: COUNTER.fetch_add(1, atomic::Ordering::Relaxed),
+        }
+    }
+}
+
+struct FieldIdSupplier {
+    current: usize,
+}
+impl FieldIdSupplier {
+    fn new() -> FieldIdSupplier {
+        FieldIdSupplier { current: 0 }
+    }
+    fn next(&mut self) -> usize {
+        let result = self.current;
+        self.current += 1;
+        result
+    }
+}
+
+pub(super) struct DeductionTable {
+    id: TableId,
+    name: String,
+    fields: Vec<Field>,
+}
+impl DeductionTable {
+    fn new(
+        name: String,
+        field_types: Vec<(String, TODO_FieldTypeStandIn)>,
+    ) -> (DeductionTable, Vec<ArrayTable>) {
+        let id = TableId::new();
+        let mut field_id_supplier = FieldIdSupplier::new();
+        let (fields, mut array_tables) = field_types
+            .iter()
+            .filter_map(|(name, field_type)| match field_type {
+                TODO_FieldTypeStandIn::Array(_flat_field_type) => None,
+                TODO_FieldTypeStandIn::Flat(flat_field_tiype) => Some(Field::new(
+                    name,
+                    id,
+                    &mut field_id_supplier,
+                    flat_field_tiype,
+                )),
+            })
+            .fold(
+                (vec![], vec![]),
+                |(mut field_acc, mut array_acc), (field_item, mut array_tables_item)| {
+                    field_acc.push(field_item);
+                    array_acc.append(&mut array_tables_item);
+                    (field_acc, array_acc)
+                },
+            );
+        array_tables.append(
+            &mut field_types
+                .iter()
+                .filter_map(|(field_name, field_type)| match field_type {
+                    TODO_FieldTypeStandIn::Array(field_type) => {
+                        Some(ArrayTable::new(field_name, id, field_type))
+                    }
+                    TODO_FieldTypeStandIn::Flat(_todo_flat_field_type_stand_in) => None,
+                })
+                .concat(),
+        );
+        (DeductionTable { id, name, fields }, array_tables)
+    }
+}
+
+pub(super) struct ArrayTable {
+    id: TableId,
+    ref_id: TableId,
+    field: Field,
+}
+impl ArrayTable {
+    fn new(
+        name: &String,
+        ref_id: TableId,
+        field_type: &TODO_FlatFieldTypeStandIn,
+    ) -> Vec<ArrayTable> {
+        let id = TableId::new();
+        let (field, mut array_tables) =
+            Field::new(name, id, &mut FieldIdSupplier::new(), field_type);
+        array_tables.push(ArrayTable { id, ref_id, field });
+        return array_tables;
+    }
+}
+
+enum Field {
+    Primitive {
+        id: usize,
+        name: String,
+        field_type: FieldType,
+    },
+    Object {
+        name: String,
+        fields: Vec<Field>,
+    },
+}
+impl Field {
+    fn new(
+        name: &String,
+        ref_id: TableId,
+        id_supplier: &mut FieldIdSupplier,
+        field_type: &TODO_FlatFieldTypeStandIn,
+    ) -> (Field, Vec<ArrayTable>) {
+        match field_type {
+            TODO_FlatFieldTypeStandIn::Object(items) => {
+                let (fields, mut array_tables) = items
+                    .iter()
+                    .filter_map(|(field_name, field_type)| match field_type {
+                        TODO_FieldTypeStandIn::Array(_field_type) => None,
+                        TODO_FieldTypeStandIn::Flat(todo_flat_field_type_stand_in) => {
+                            Some(Field::new(
+                                field_name,
+                                ref_id,
+                                id_supplier,
+                                todo_flat_field_type_stand_in,
+                            ))
+                        }
+                    })
+                    .fold(
+                        (vec![], vec![]),
+                        |(mut field_acc, mut array_acc), (field_item, mut array_tables_item)| {
+                            field_acc.push(field_item);
+                            array_acc.append(&mut array_tables_item);
+                            (field_acc, array_acc)
+                        },
+                    );
+                array_tables.append(
+                    &mut items
+                        .iter()
+                        .filter_map(|(field_name, field_type)| match field_type {
+                            TODO_FieldTypeStandIn::Array(field_type) => {
+                                Some(ArrayTable::new(field_name, ref_id, field_type))
+                            }
+                            TODO_FieldTypeStandIn::Flat(_todo_flat_field_type_stand_in) => None,
+                        })
+                        .concat(),
+                );
+                (
+                    Field::Object {
+                        name: name.clone(),
+                        fields,
+                    },
+                    array_tables,
+                )
+            }
+            TODO_FlatFieldTypeStandIn::Primitive(field_type) => (
+                Field::Primitive {
+                    id: id_supplier.next(),
+                    name: name.clone(),
+                    field_type: field_type.clone(),
+                },
+                vec![],
+            ),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+enum FieldType {
+    Number,
+    Boolean,
+    Enum(Vec<String>),
+}
+impl FieldType {}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+enum TODO_FieldTypeStandIn {
+    Array(TODO_FlatFieldTypeStandIn),
+    Flat(TODO_FlatFieldTypeStandIn),
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+enum TODO_FlatFieldTypeStandIn {
+    Object(Vec<(String, TODO_FieldTypeStandIn)>),
+    Primitive(FieldType),
+}
--- crates/puzzle_pack_bindings/src/lib.rs
+++ crates/puzzle_pack_bindings/src/lib.rs
@@ -1,1 +1,11 @@
 pub mod ts_api;
+
+mod layers {
+    pub(super) mod first_layer {
+        pub(super) struct Rule {
+        }
+        pub(super) struct Deduction {}
+        pub(super) struct LogicStep {}
+    }
+    pub(super) mod second_layer;
+}
